832a8fcc3d8929cf4eb54bc69d95b2c5028ef968
Siqve <aadne8@gmail.com>
2024-05-13 17:40:58 +0200

    Created HueTab

---
+++ src/containers/HueTab.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { Children, ReactNode, useState } from "react";
+import clsx from "clsx";
+
+export type HueTabProps = {
+    tabNames: string[];
+    children: ReactNode;
+};
+
+export const HueTab = ({ tabNames, children }: HueTabProps) => {
+    const [activeTabIndex, setActiveTabIndex] = useState<number>(0);
+    if (tabNames.length === 0) return null;
+    const childrenArray = Children.toArray(children);
+
+    return (
+        <div className="flex w-full flex-col text-center">
+            <div className="flex justify-center gap-5 pb-5">
+                {tabNames.map((tabName, index) => (
+                    <button
+                        key={tabName}
+                        className={clsx(
+                            "text-3xl",
+                            activeTabIndex === index
+                                ? "border-b border-b-text-main text-base-content"
+                                : "text-gray-950",
+                        )}
+                        onClick={() => {
+                            setActiveTabIndex(index);
+                        }}
+                    >
+                        {tabName}
+                    </button>
+                ))}
+            </div>
+            {childrenArray[activeTabIndex]}
+        </div>
+    );
+};
