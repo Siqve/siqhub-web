868460264f936e0608d54b130ed6446f1e7deeab
Siqve <aadne8@gmail.com>
2024-05-18 16:47:46 +0200

    Added route listener for event-stream on settings updated

---
+++ src/app/api/firestore/on-settings-update/route.ts
@@ -0,0 +1,33 @@
+import { createSettingsListener } from "@/libs/firebase/queries/createSettingsListener";
+import { Unsubscribe } from "@firebase/firestore";
+
+const headers = {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+};
+
+export async function GET(request: Request) {
+    let unsubscribe: Unsubscribe;
+    return new Response(
+        new ReadableStream({
+            async start(controller) {
+                unsubscribe = createSettingsListener((settings) => {
+                    controller.enqueue(
+                        `data: ${JSON.stringify(settings)}\n\n`,
+                    );
+                });
+                request.signal.addEventListener("abort", () => {
+                    unsubscribe();
+                    controller.close();
+                });
+            },
+            async cancel() {
+                if (unsubscribe) {
+                    unsubscribe();
+                }
+            },
+        }),
+        { headers },
+    );
+}
