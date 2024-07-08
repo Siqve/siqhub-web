684b9751dcf7ed3ed900f94056fcbac7dc7fa8e6
Siqve <aadne8@gmail.com>
2024-07-08 20:32:52 +0200

    Prettiering

--- src/app/device/[id]/page.tsx
+++ src/app/device/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { Content } from "@/containers/Content";
 import { getDevice } from "@/managers/DeviceManager";
-import { DeviceHeader } from "@components/device/DeviceHeader";
 import { LedStripController } from "@components/controllers/LedStripController";
+import { DeviceHeader } from "@components/device/DeviceHeader";
 import { redirect } from "next/navigation";
 
 const DevicePage = async ({ params }: { params: { id: string } }) => {
