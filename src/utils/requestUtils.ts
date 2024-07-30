4fbd552b628b10ac2366c91c707df87c1bf18df4
Siqve <aadne8@gmail.com>
2024-07-30 20:03:09 +0200

    Added createEventSource as request util func

--- src/utils/requestUtils.ts
+++ src/utils/requestUtils.ts
@@ -1,3 +1,14 @@
 export const isServer = () => typeof window === "undefined";
 
 export const getURLOrigin = (): string => window.location.origin;
+
+export const createEventSource = (
+    url: string,
+    callback: (event: MessageEvent<any>) => void,
+): EventSource => {
+    if (isServer()) throw new Error("EventSource is not supported on server side.");
+
+    const eventSource = new EventSource(url);
+    eventSource.onmessage = callback;
+    return eventSource;
+};
