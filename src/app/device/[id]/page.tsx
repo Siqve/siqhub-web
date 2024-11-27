a00b75e1524c42c4d090f4cc2f4ce14aa50858e0
Siqve <aadne8@gmail.com>
2024-11-27 17:28:01 +0100

    Refactored searchparam usage

--- src/app/device/[id]/page.tsx
+++ src/app/device/[id]/page.tsx
@@ -1,11 +1,17 @@
 import { Content } from "@/containers/Content";
+import { db } from "@/services/dbService";
 import { LedStripController } from "@components/controllers/LedStripController";
 import { DeviceHeader } from "@components/device/DeviceHeader";
 import { redirect } from "next/navigation";
-import { db } from "@/services/dbService";
 
-const DevicePage = async ({ params }: { params: { id: string } }) => {
-    const device = await db.table().device().select(params.id);
+type DevicePageProps = {
+    searchParams?: { [key: string]: string | string[] | undefined };
+};
+
+const DevicePage = async ({ searchParams }: DevicePageProps) => {
+    if (typeof searchParams?.id !== "string") redirect("/");
+
+    const device = await db.table().device().select(searchParams.id);
     if (!device) redirect("/");
 
     return (
